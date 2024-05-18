939538fb73cbecc932dcbf0827e7cfb320221ff6
VikashKrPandit <vixcypandit22@gmail.com>
2024-05-18 15:55:52 +0530

    db update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '154.49.247.11',
-    user: 'u902679059_op',
-    password: '@OPlottery008',
-    database: 'u902679059_op'
+    host: 'localhost',
+    user: 'root',
+    password: 'Numolo@12345',
+    database: 'ninja'
 });
 
 // const connection = mysql.createPool({
